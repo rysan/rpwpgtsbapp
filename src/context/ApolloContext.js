5703d1636358937ec4a4c23dde36f89ecf487ab4
Andriy <a.rysan@yahoo.com>
2020-04-21 03:27:56 +0300

    updates

--- src/context/ApolloContext.js
+++ src/context/ApolloContext.js
@@ -1,5 +1,11 @@
+import React from 'react'
+import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'apollo-boost';
 
 export const client = new ApolloClient({
   uri: `https://qarunpanther.wpengine.com/graphql`,
 })
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+)
